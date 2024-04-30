38130e4d4ce8a96436c785c955ae5488d374f7ce
DESKTOP-67BT1F3\Panda <chrisrberg03@gmail.com>
2024-04-30 13:55:52 +0200

    Created a registration form that take the values in the input and sends them to a fetch

---
+++ js/register.js
@@ -0,0 +1,74 @@
+const main = document.getElementById("mainContainer");
+const registerBTN = document.getElementById("register");
+
+function validateImageFields(avatarUrl, avatarAlt, bannerUrl, bannerAlt) {
+    if ((!avatarUrl || !avatarAlt) && (!bannerUrl || !bannerAlt)) {
+        return false;
+    }
+    return true;
+}
+
+registerBTN.addEventListener("click", async () => {
+    const name = document.getElementById("userName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const bio = document.getElementById("bio").value;
+    const avatarUrl = document.getElementById("avatarUrl").value;
+    const avatarAlt = document.getElementById("avatarAlt").value;
+    const bannerUrl = document.getElementById("bannerUrl").value;
+    const bannerAlt = document.getElementById("bannerAlt").value;
+    console.log(name, bio, avatarUrl, avatarAlt, bannerUrl, bannerAlt);
+
+    const requestBody = {
+        name: name,
+        email: email,
+        password: password,
+        bio: bio,
+    };
+
+    if (validateImageFields(avatarUrl, avatarAlt, bannerUrl, bannerAlt)) {
+        if (avatarUrl && avatarAlt) {
+            requestBody.avatar = {
+                url: avatarUrl,
+                alt: avatarAlt,
+            };
+        }
+
+        if (bannerUrl && bannerAlt) {
+            requestBody.banner = {
+                url: bannerUrl,
+                alt: bannerAlt,
+            };
+        }
+    }
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+    };
+
+    try {
+        const response = await fetch(
+            "https://v2.api.noroff.dev/auth/register",
+            requestOptions
+        );
+
+        if (!response.ok) {
+            if (response.status === 400) {
+                alert("Please enter a valid email");
+            } else {
+                throw new Error("Failed to register");
+            }
+        }
+        const data = await response.json();
+        if (response.status === 201) {
+            console.log("Registration successful", data);
+            window.location.href = "/account/login.html";
+        }
+    } catch (error) {
+        console.error("Login error:", error);
+    }
+});
